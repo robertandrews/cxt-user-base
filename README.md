8176e4714b58d28e2cf06f22cd4bb731296f309a
robertandrews <mail@robertandrews.co.uk>
2022-03-10 21:15:00 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Context WP User Base Changer
+
+Tested up to: 5.9.1  
+Tags: users, user  
+Contributors: robertandrews  
+
+## Description
+
+Customise URL base of WordPress author archive indexes.
+Change the default http://example.com/author/bill-gates/ to http://example.com/custom/bill-gates/
+
+## Settings
+
+Requires setting one variable in the plugin code:
+
+`$new_base = 'user';`
+
+## Acknowledgements
+
+[Code originates](http://wpsnipp.com/index.php/functions-php/change-the-author-slug-url-base/) from Debjit Saha, isitwp
+
+## Notes
+
+This plugin stores no database information directly.
---
+++ cxt-user-base.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name:    Context User Base Changer
+Description:    Customise URL base of author archive indexes.
+Text-Domain:    cwp-user-base
+Version:        1.1
+Author:         Robert Andrews
+Author URI:     http://www.robertandrews.co.uk
+License:        GPL v2 or later
+License URI:    https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// Set the new URL base, eg. http://example.com/{new_base}/bill-gates/
+$new_base = 'user';
+
+/**
+ * Acknowledgement:
+ * Code by Debjit Saha, isitwp
+ * http://wpsnipp.com/index.php/functions-php/change-the-author-slug-url-base/
+ */
+add_action('init', 'change_author_base');
+function change_author_base()
+{
+    global $wp_rewrite;
+    $author_slug = $new_base; // change slug name
+    $wp_rewrite->author_base = $author_slug;
+}
+
+?>
